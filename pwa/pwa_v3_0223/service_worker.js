97320a43bf3e3ef4abfebf8d92b0b558f21885ef
Tetsu <tktkchestnut@gmail.com>
2021-02-23 10:17:47 +0900

    new version!

---
+++ pwa/pwa_v3_0223/service_worker.js
@@ -0,0 +1,30 @@
+// キャッシュファイルの指定
+var CACHE_NAME = 'pwa-sample-caches';
+var urlsToCache = [
+    '/index.html',
+    '/image/icon_192.png',
+    '/image/icon_256.png'
+];
+
+// インストール処理
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response ? response : fetch(event.request);
+            })
+    );
+});
+

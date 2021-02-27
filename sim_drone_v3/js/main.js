d6d5b547518c50d1e65acbadb1573e09ac3d2b88
Tetsu <tktkchestnut@gmail.com>
2021-02-27 11:32:06 +0900

    v4

--- sim_drone_v3/js/main.js
+++ sim_drone_v3/js/main.js
@@ -1,9 +1,8 @@
-class ImageController {
-    constructor() {
-      this.x = 150;
-      this.y = 150;
-      this.scale = 1;
-      this.speed = 1;
-      this.directionRotation = '0';
-    }
-}
+var sim_time = 0;
+var real_time = 0;
+
+//シミュレーション用変数
+let sim_pos_x, sim_pos_y , sim_pos_z; 
+let sim_vel_x, sim_vel_y , sim_vel_z; 
+let sim_ang_x, sim_ang_y , sim_ang_z;
+let sim_rate_x, sim_rate_y, sim_rate_z;

ca5a1ac6c2c250a08a574922239b7887df6d1840
Tetsu <tktkchestnut@gmail.com>
2021-02-28 10:06:08 +0900

    v5.0 button

---
+++ sim_drone_v5/js/main.js
@@ -0,0 +1,8 @@
+var sim_time = 0;
+var real_time = 0;
+
+//シミュレーション用変数
+let sim_pos_x, sim_pos_y , sim_pos_z; 
+let sim_vel_x, sim_vel_y , sim_vel_z; 
+let sim_ang_x, sim_ang_y , sim_ang_z;
+let sim_rate_x, sim_rate_y, sim_rate_z;
